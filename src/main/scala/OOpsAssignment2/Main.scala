05a6684229e2f1485ab2dc3cc17f31f611545ebc
uditknolder <86972851+uditknolder@users.noreply.github.com>
2021-08-06 11:33:45 +0530

    Update Main.scala

--- src/main/scala/OOpsAssignment2/Main.scala
+++ src/main/scala/OOpsAssignment2/Main.scala
@@ -7,29 +7,32 @@ object Main {
   def main(args: Array[String]): Unit =
   {
 
-    val Person1=new Person(24,"test")
+    val Person1=new Person(24,"testagain")
     val Person2=new Person(25,"test")
 
 
-    if ((Person1.name).equals(Person1.name))
+    if ((Person1.name).equals(Person2.name))
       {
-        if((Person1.age)equals(Person2.age))
+        if((Person1.age)<(Person2.age))
           {
             println("true")
           }
           else
           {
-            println("true")
+            println("false")
           }
 
       }
-    else if ((Person2.name.length()) equals (Person1.name.length()))
-    {
-      println("false")
-    }
     else
       {
-        println("false")
+        if ((Person1.name.length()) < (Person2.name.length()))
+        {
+          println("true")
+        }
+        else
+        {
+          println("false")
+        }
       }
 
    }
